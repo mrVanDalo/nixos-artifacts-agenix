dd7c6e279a6324d7d3e4fb6d0c81cdfb6b4974fd
Ingolf Wagner <contact@ingolf-wagner.de>
2025-09-06 12:02:19 +0200

    :wrench: add mode parameter to files

--- modules/store.nix
+++ modules/store.nix
@@ -20,7 +20,7 @@
               lib.nameValuePair "${artifact.name}-${file.name}" {
                 # fixme : config.networking.hostName might not be correct
                 file = "${config.artifacts.config.agenix.flakeStoreDir}/per-machine/${config.artifacts.config.agenix.machineName}/${artifact.name}/${file.name}.age";
-                inherit (file) owner group path;
+                inherit (file) owner group path mode;
               }
             ) (lib.attrValues artifact.files)
           ) (lib.attrValues config.artifacts.store)
